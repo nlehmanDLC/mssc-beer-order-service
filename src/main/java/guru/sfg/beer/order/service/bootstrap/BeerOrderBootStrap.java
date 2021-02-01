7a34c1c66ad0989fe5f3df63ba22b42314855b1d
Nathan Lehman <nlehman@dlcouch.com>
2021-02-01 10:59:00 -0500

    setup mysql

--- src/main/java/guru/sfg/beer/order/service/bootstrap/BeerOrderBootStrap.java
+++ src/main/java/guru/sfg/beer/order/service/bootstrap/BeerOrderBootStrap.java
@@ -17,8 +17,8 @@ import java.util.UUID;
 @Component
 public class BeerOrderBootStrap implements CommandLineRunner {
     public static final String TASTING_ROOM = "Tasting Room";
-    public static final String BEER_1_UPC = "0631234200036";
-    public static final String BEER_2_UPC = "0631234300019";
+    public static final String BEER_1_UPC = "0632134200036";
+    public static final String BEER_2_UPC = "0632134300019";
     public static final String BEER_3_UPC = "0083783375213";
 
     private final CustomerRepository customerRepository;
